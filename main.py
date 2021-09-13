28ee967437d774f19c75dd93fa44ab907fcc40f6
thanathip <thanathipch9@gmail.com>
2021-09-14 01:25:03 +0700

    ⚡️Add function apply , group

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 
 def send_message(data_list):
-    url_line = 'https://notify-api.line.me/api/notify'
+    url_line = str(env['URL_LINE'])
     token = str(env['LINE_TOKEN'])
     headers = {'content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + token}
     now = datetime.now()
@@ -31,7 +31,7 @@ def send_message(data_list):
 
 
 def send_msg(message):
-    url_line = 'https://notify-api.line.me/api/notify'
+    url_line = str(env['URL_LINE'])
     token = str(env['LINE_TOKEN'])
     headers = {'content-type': 'application/x-www-form-urlencoded', 'Authorization': 'Bearer ' + token}
     now = datetime.now()
@@ -48,11 +48,11 @@ def send_msg(message):
     print(r.text, dt_string)
 
 
-def scarecrow(_id):
-    url_apply = "https://backend-farm.plantvsundead.com/farms/apply-tool"
+def apply_tools(_id, toolId):
+    url_apply = f"{str(env['URL_PVU'])}/farms/apply-tool"
     payload_apply = json.dumps({
         "farmId": _id,
-        "toolId": 4,
+        "toolId": toolId,
         "token": {
             "challenge": "default",
             "seccode": "default",
@@ -60,15 +60,46 @@ def scarecrow(_id):
         }
     })
     headers_apply = {
+        'Authorization': str(env['TOKEN']),
         'Content-Type': 'application/json'
     }
 
     response_apply = requests.request("POST", url, headers=headers_apply, data=url_apply)
+    if response_apply.status_code == 200:
+        if response_apply.json()['status'] == 0:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+def group():
+    url_group = f"{str(env['URL_PVU'])}/farm-status"
+    headers = {
+        'Authorization': str(env['TOKEN'])
+    }
+    payload = {}
+    start_time = time.time()
+    rq = requests.request("GET", url_group, headers=headers, data=payload)
+    if rq.status_code == 200:
+        if rq.json()['status'] == 0:
+            data_x = rq.json()['data']
+            status = data_x['status']
+            if 'inGroup' in data_x and 'currentGroup' in data_x:
+                in_group = rq.json()['data']['inGroup']
+                currentGroup = rq.json()['data']['currentGroup']
+                totalGroup = rq.json()['data']['totalGroup']
+                print(f"{datetime.now().strftime('%d/%m/%Y %H:%M:%S')} |⚡️ your group {in_group} | 🔥 current group {currentGroup} / {totalGroup} | 🧾 load {round(time.time() - start_time,5)} ms.")
+            elif rq.json()['status'] == 0 and status == 0:
+                print("⚡️ Farm Maintenance")
+            else:
+                pass
+            return status
 
 
 if __name__ == '__main__':
-    url = "https://backend-farm.plantvsundead.com/farms?limit=10&offset=0"
-    url_group = "https://backend-farm.plantvsundead.com/farm-status"
+    url = f"{str(env['URL_PVU'])}/farms?limit=10&offset=0"
     payload = {}
     headers = {
         'Authorization': str(env['TOKEN'])
@@ -77,57 +108,58 @@ if __name__ == '__main__':
     count_water = 0
     seed = 0
     crow = 0
-    is_notify = False
+    is_notify_group = False
+    is_notify_msg = False
+    old_data = {}
     while True:
-        rq = requests.request("GET", url_group, headers=headers, data=payload)
-        if rq.status_code == 200:
-            if rq.json()['status'] == 0:
-                data_x = rq.json()['data']
-                # print(data_x)
-                if 'inGroup' in data_x and 'currentGroup' in data_x:
-                    in_group = rq.json()['data']['inGroup']
-                    currentGroup = rq.json()['data']['currentGroup']
-                    print(f'current group {currentGroup} , your group {in_group} ')
+        check_group = group()
+        if check_group == 0:
+            is_notify_group = False
+        if check_group == 1:
+            if is_notify_group is False:
+                send_msg(f" สามารถเข้า Plan vs Undead ได้")
+                is_notify_group = True
 
-                status = rq.json()['data']['status']
-                if status == 0:
-                    is_notify = False
-                if status == 1:
-                    if is_notify is False:
-                        send_msg(f" สามารถเข้า Plan vs Undead ได้")
-                        is_notify = True
-                    response = requests.request("GET", url, headers=headers, data=payload)
-                    start_time = time.time()
-                    data_plant = []
-                    if response.status_code == 200:
-                        data_source = response.json()
-                        # print(data_source)
-                        if data_source['status'] == 0:
-                            data = data_source['data']
-                            for i in data:
-                                if i['needWater'] is False:
-                                    count_water = count_water + 1
-                                if i['totalHarvest'] > 0:
-                                    le = le + i['totalHarvest']
-                                if i['hasSeed'] is True:
-                                    seed = seed + 1
-                                if 'hasCrow' in i:
-                                    if i['hasCrow'] is True:
-                                        crow = crow + 1
-                                        # scarecrow(i['_id'])
-                        if le != 0 or count_water != 0 or seed != 0 or crow != 0:
-                            send_message({'le': le, 'seed': seed, 'water': count_water, 'crow': crow})
-                            le = 0
-                            seed = 0
-                            count_water = 0
-                            crow = 0
-                        now = datetime.now()
-                        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-                        success_msg = '''request success {time} , {ms} ms'''.format(time=dt_string,
-                                                                                    ms=round(time.time() - start_time,
-                                                                                             5))
-                        print(success_msg)
-                    else:
-                        pass
-                    time.sleep(360)
-        time.sleep(10)
+            response = requests.request("GET", url, headers=headers, data=payload)
+            start_time = time.time()
+            data_plant = []
+            if response.status_code == 200:
+                data_source = response.json()
+                # print(data_source)
+                if data_source['status'] == 0:
+                    data = data_source['data']
+                    for i in data:
+                        if i['needWater'] is True:
+                            count_water = count_water + 1
+                            # apply_tools(i['_id'],3)
+                        if i['totalHarvest'] > 0:
+                            le = le + i['totalHarvest']
+                        if i['hasSeed'] is True:
+                            seed = seed + 1
+                        if 'hasCrow' in i:
+                            if i['hasCrow'] is True:
+                                crow = crow + 1
+                                # apply_tools(i['_id'],4)
+                _data = {'le': le, 'seed': seed, 'water': count_water, 'crow': crow}
+                if old_data == _data:
+                    is_notify_msg = True
+                if le != 0 or count_water != 0 or seed != 0 or crow != 0:
+                    if is_notify_msg is False:
+                        send_message(_data)
+                        old_data = _data.copy()
+                        le = 0
+                        seed = 0
+                        count_water = 0
+                        crow = 0
+                else:
+                    now = datetime.now()
+                    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+                    success_msg = ''' ⚡️ {time} request success  , 🧾 load {ms} ms'''.format(time=dt_string,
+                                                                                ms=round(time.time() - start_time,
+                                                                                         5))
+                    print(success_msg)
+                time.sleep(300)
+            else:
+                pass
+        else:
+            time.sleep(30)
